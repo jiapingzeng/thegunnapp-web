d338c86ed88fa72bf60a5eb93a2d0b67d8544272
Jiaping Zeng <jiaping@gisar.me>
2018-01-19 18:59:44 -0800

    o

--- app.js
+++ app.js
@@ -64,8 +64,8 @@ app.post('/list', (req, res, next) => {
 
 
 function updateFile(cb) {
-    getCalendar((data) => {
-        fs.writeFile('./public/schedule.json', data, (err) => {
+    getSchedule((data) => {
+        fs.writeFile('./public/schedule.json', JSON.stringify(data.items), (err) => {
             if (err) cb(err)
         })
     }, (err) => {
